6d066396d3ef50a1d7bb5c63da7e295e08d238f8
Jamesguo <591759368@qq.com>
2021-04-13 18:01:56 +0800

    first push

---
+++ provider/src/main/java/com/guojian/springcloud/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.guojian.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author James Guo
+ * @date 2021/4/13  16:02
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Cloud";
+    }
+}
