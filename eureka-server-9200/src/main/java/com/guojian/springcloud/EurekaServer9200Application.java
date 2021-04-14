115f447e38ce81555883c80383d84f7f23ad2c62
Jamesguo <591759368@qq.com>
2021-04-14 09:29:02 +0800

    push

--- eureka-server-9200/src/main/java/com/guojian/springcloud/EurekaServer9200Application.java
+++ eureka-server-9200/src/main/java/com/guojian/springcloud/EurekaServer9200Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServer9200Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaServer9200Application.class, args);
     }
 
 }
--- eureka-server-9200/src/test/java/com/guojian/springcloud/EurekaServer9200ApplicationTests.java
+++ eureka-server-9200/src/test/java/com/guojian/springcloud/EurekaServer9200ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class EurekaServer9200ApplicationTests {
 
     @Test
     void contextLoads() {
